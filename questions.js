2863e4be5eafe9a36a2c0f00618382e106708368
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-09 21:29:00 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,28 @@
+//Array of objects
+const quiz = [
+    {
+        q:"An American woman ___ a newspaper when she ___ a photo of her husband.",
+        options:["was reading / saw", "had read / saw", "read / had seen"],
+        answer: 0
+    },
+    {
+     q:"He was working in the US navy when he ___ of a crime.",
+        options:["had been accused", "was accused", "accused"],
+        answer: 1   
+    },
+    {
+        q:"He ___ in the UK when he ___ .",
+        options:["had lived / got married", "lived / had got married", "was living / got married"],
+        answer: 2
+    },
+    {
+        q:"She ___ the children that their father ___ to prison.",
+        options:["didn't tell / had gone", "wasn't telling / went", "hadn't told / went"],
+        answer: 0
+    },
+    {
+        q:"The judge ___ the teenager until she ___ to the evedence.",
+        options:["wasn't sentenced / listened", "didn't sentence / had listened", "hadn't sentenced / was listened"],
+        answer: 1
+    }
+]
